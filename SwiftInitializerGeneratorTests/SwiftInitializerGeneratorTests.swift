49fe1e865788d2bd45c1fdd86020a59ac925ed42
Bouke Haarsma <bouke@haarsma.eu>
2016-12-21 15:16:27 +0100

    Added failing test cases

--- SwiftInitializerGeneratorTests/SwiftInitializerGeneratorTests.swift
+++ SwiftInitializerGeneratorTests/SwiftInitializerGeneratorTests.swift
@@ -9,18 +9,106 @@
 import XCTest
 
 class SwiftInitializerGeneratorTests: XCTestCase {
-    func _test(input: [String], output: [String], file: StaticString = #file, line: UInt = #line) {
+    func assert(input: [String], output: [String], file: StaticString = #file, line: UInt = #line) {
         do {
             let lines = try generate(selection: input, tabWidth: 4, indentationWidth: 4)
             if(lines != output) {
-                XCTFail("Output is not correct expected \(output), got \(lines)", file: file, line: line)
+                XCTFail("Output is not correct; expected:\n\(output.joined(separator: "\n"))\n\ngot:\n\(lines.joined(separator: "\n"))", file: file, line: line)
             }
         } catch {
-            XCTFail("Could not generate initializer", file: file, line: line)
+            XCTFail("Could not generate initializer: \(error)", file: file, line: line)
         }
     }
 
-    func testNoAccessModifiers() throws {
-        _test(input: ["let a: Int", "let b: Int"], output: [])
+    func testNoAccessModifiers() {
+        assert(
+            input: [
+                "let a: Int",
+                "let b: Int"
+            ],
+            output: [
+                "    public init(a: Int, b: Int) {",
+                "        self.a = a",
+                "        self.b = b",
+                "    }"
+            ])
+    }
+
+    func testNoProperties() {
+        assert(
+            input: [
+                "",
+                ""
+            ],
+            output: [
+                "    public init(a: Int, b: Int) {",
+                "        self.a = a",
+                "        self.b = b",
+                "    }"
+            ])
+    }
+
+    func testEmptyLineInBetween() {
+        assert(
+            input: [
+                "let a: Int",
+                "",
+                "let b: Int"
+            ],
+            output: [
+                "    public init(a: Int, b: Int) {",
+                "        self.a = a",
+                "        self.b = b",
+                "    }"
+            ])
+    }
+
+    func testSingleAccessModifier() {
+        assert(
+            input: [
+                "internal let a: Int",
+                "private let b: Int"
+            ],
+            output: [
+                "    public init(a: Int, b: Int) {",
+                "        self.a = a",
+                "        self.b = b",
+                "    }"
+            ])
+    }
+
+    func testDoubleAccessModifier() {
+        assert(
+            input: [
+                "public internal(set) let a: Int",
+                "public private(set) let b: Int"
+            ],
+            output: [
+                "    public init(a: Int, b: Int) {",
+                "        self.a = a",
+                "        self.b = b",
+                "    }"
+            ])
+    }
+
+
+    func testCommentLine() {
+        assert(
+            input: [
+                "/// a very important property",
+                "let a: Int",
+                "// this one, not so much",
+                "let b: Int",
+                "/*",
+                " * pay attention to this one",
+                " */",
+                "let c: IBOutlet!"
+            ],
+            output: [
+                "    public init(a: Int, b: Int) {",
+                "        self.a = a",
+                "        self.b = b",
+                "    }"
+            ])
     }
 }
